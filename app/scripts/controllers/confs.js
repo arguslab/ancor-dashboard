3513709fcd3e1b2fbb3aed79703dddac5e16f13a
Brian Cain <brianccain@gmail.com>
2014-02-26 13:00:52 -0600

    Modify new controller to display loading config notice
    This commit modifies the controller confs to show a 'loading
    configurations' notification while the controller works in the
    background. Currently there are no HTTP GET calls to ancor to retrieve
    the information, so this will be a place holder.

--- app/scripts/controllers/confs.js
+++ app/scripts/controllers/confs.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('ancorDashboardApp')
-  .controller('ConfsCtrl', function ($scope) {
-    $scope.title = "ANCOR Configurations";
+  .controller('ConfsCtrl', function ($scope, $location, $http) {
+    $scope.title = 'ANCOR Configurations';
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    // $scope.data = [{name: 'My Title', contents: 'Contents'}, {name: 'Title 2', contents: 'Woot woot woot'}];
+
+    $scope.data = [{name: 'Loading posts...', contents: ''}];
   });
