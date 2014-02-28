352bf578df15663422b792a19905f23f11330488
Brian Cain <brianccain@gmail.com>
2014-02-28 10:39:56 -0600

    Add modal view to instance details
    This commit adds a modal view for each instance within the instance
    details table. For now it will just display the simple information
    provided in instances.json, but in the future it will do an HTTP GET
    call on the specific instance ID to show more information in the modal
    about the given instance.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('ancorDashboardApp')
-  .controller('MainCtrl', function ($scope, $http, $window) {
+  .controller('MainCtrl', function ($scope, $http, $window, $modal, $log) {
     $http.get('ancor-api-sample/instances.json').success(function(data) {
       $scope.instances = data;
     });
+
+    // $scope.items = ['item1', 'item2', 'item3'];
 
     // GET api/instances/x
     // Query ancor for specific instance for detailed view
@@ -19,6 +21,27 @@ angular.module('ancorDashboardApp')
       $window.alert(str);
     };
 
+    // Modal view of a given instance
+    $scope.open = function (instance) {
+      $scope.items = instance;
+      console.log(instance);
+      var modalInstance = $modal.open({
+        templateUrl: 'myModalContent.html',
+        controller: ModalInstanceCtrl,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
     $scope.title = 'ANCOR Index';
     $scope.version = '0.0.1'; // will be replaced by HTTP GET /api/version
     $scope.awesomeThings = [
@@ -30,3 +53,18 @@ angular.module('ancorDashboardApp')
 
     // $scope.instances = [{name: 'Test Name', interfaces: 'Test Interface', stage: 'Success'}, {name: 'Test 2', interfaces: '2Int 2Face', stage: 'Success'}];
   });
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+}
