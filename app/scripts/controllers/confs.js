81ef6373b87a246a594d602c8a79fc3a1d5cd48c
Brian Cain <brianccain@gmail.com>
2014-03-11 11:44:43 -0500

    Add ace editor with configuration

--- app/scripts/controllers/confs.js
+++ app/scripts/controllers/confs.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ancorDashboardApp')
-  .controller('ConfsCtrl', function ($scope) {
+  .controller('ConfsCtrl', function ($scope, $window, $http) {
     // $http.get('ancor-api-sample/instances.json').success(function(data) {
     //   // get current config
     // )};
@@ -19,6 +19,33 @@ angular.module('ancorDashboardApp')
 
     $scope.data = [{name: 'Loading posts...', contents: ''}];
 
+    // Load config
+    $scope.confData = 'empty';
+
+    var loadConf = function(_editor) {
+      $http.get('conf-sample/fullstack.yaml').success(function(data) {
+        // var _session = _editor.getSession();
+        $scope.confData = data;
+        _editor.insert($scope.confData);
+        _editor.navigateFileStart();
+        // console.log($scope.confData);
+      });
+    };
+
+    $scope.loadConf = function(_editor) {
+      var _session = _editor.getSession();
+      // var _renderer = _editor.renderer;
+
+      _session.setUndoManager(new ace.UndoManager());
+
+      loadConf(_editor);
+      // _editor.setValue($scope.confData);
+      // console.log($scope.confData);
+    };
+
     // POST /api/deploy
     // Take given yaml file from dashboard and send to ancor
+    $scope.deploy = function (msg) {
+      $window.alert(msg);
+    };
   });
