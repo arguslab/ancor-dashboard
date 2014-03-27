e627de8d50143710b115bbc3c40836e1a23fc1ba
Brian Cain <brianccain@gmail.com>
2014-03-27 14:32:51 -0500

    Modify tasks controller/view
    Add labels to state, continue to develop task page

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -12,17 +12,35 @@ angular.module('ancorDashboardApp')
       $scope.version = data.version;
     });
 
+    $scope.quantity = 15;
 
-    $http.get($rootScope.ancorIPAddress+'/v1/tasks').success(function(data) {
-      $scope.tasks = data;
-      $scope.keys = [];
+    $scope.getData = function() {
+      $http.get($rootScope.ancorIPAddress+'/v1/tasks').success(function(data) {
+        $scope.tasks = data;
+        $scope.keys = [];
 
-      angular.forEach($scope.tasks[0], function(value, key) {
-        $scope.keys.push(key);
+        angular.forEach($scope.tasks[0], function(value, key) {
+          $scope.keys.push(key);
+        });
+
+        $scope.predicate = '-updated_at';
       });
+    };
 
-      $scope.predicate = '-updated_at';
-    });
+    $scope.getData();
+
+    // Function to replicate setInterval using $timeout service.
+    //
+    // Problem with this is it gets called when user isn't on tasks page
+    // $scope.intervalFunction = function(){
+    //   $timeout(function() {
+    //     $scope.getData();
+    //     $scope.intervalFunction();
+    //   }, 10000);
+    // };
+
+    // Kick off the interval
+    // $scope.intervalFunction();
 
     $scope.title = 'ANCOR Tasks';
   });
